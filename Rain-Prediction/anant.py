a9998ca7029ce6c8ed6cc04f925598025198d25a
ANANT AGGARWAL <74396966+ANANT-2004@users.noreply.github.com>
2025-03-11 17:07:41 +0530

    Add files via upload

---
+++ Rain-Prediction/anant.py
@@ -0,0 +1,17 @@
+return value="true "
+append a data list where column size="null" and product size== "10";
+
+
+
+for i in range (10.20)[range]{
+    loop continues till execution and implement the function void();
+    for class in variables to showcase looping standards append the list as follows 
+
+
+
+
+
+    for subject to be classified under the category IndentationError
+    
+
+}
